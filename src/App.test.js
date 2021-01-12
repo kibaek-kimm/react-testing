eee7835cbc601c920401cd2ff8b59774db221e16
Kibaek Kim <kibaek.kimm@gmail.com>
2021-01-13 01:08:38 +0900

    shallow.debug

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,13 @@
 import React from "react"
-import Enzyme from "enzyme";
+import Enzyme, { shallow } from "enzyme";
 import EnzymeAdapter from "enzyme-adapter-react-16"
 import App from "./App";
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
-test("renders learn react link", () => {
+test("renders without crashing", () => {
+  const wrapper = shallow(<App />);
   
+  // 디버깅을 목적으로 렌더링된 소스코드를 반환한다.
+  // console.log(wrapper.debug());
 });
