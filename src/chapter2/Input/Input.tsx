14302fccfba0d7c905b10c1ccb4cb5991ebade2b
Kibaek Kim <kibaek.kimm@gmail.com>
2021-07-05 00:29:31 +0900

    fix: material ui적용, Input 컴포넌트 구현(as controlled component)

--- src/chapter2/Input/Input.tsx
+++ src/chapter2/Input/Input.tsx
@@ -1,9 +1,28 @@
-interface IProps {
+import { Button, Input as MUIInput } from "@material-ui/core";
+import React from "react"
 
+interface IProps {
+  secretWord: string;
 }
 
 const Input: React.FC<IProps> = (props) => {
-  return <input type="text" data-test="component-input" />
+  const [currentGuess, setCurrentGuess] = React.useState("");
+  return <div data-test="component-input" style={{ marginBottom: "24px" }}>
+    <form>
+      <MUIInput 
+        data-test="input-box"
+        type="text"
+        placeholder="Enter guess"
+        value={currentGuess}
+        onChange={e => setCurrentGuess(e.target.value)}
+      />
+      <Button 
+        data-test="submit-button"
+      >
+        Submit 
+      </Button>
+    </form>
+  </div>
 }
 
 export default Input
