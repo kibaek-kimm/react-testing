871fa33ecadd3b421424f4104f1120f813526087
Kibaek Kim <kibaek.kimm@gmail.com>
2021-07-05 00:30:22 +0900

    feat: Input 컴포넌트 onChange 테스트코드 추가

--- src/chapter2/Input/Input.test.tsx
+++ src/chapter2/Input/Input.test.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { shallow } from "enzyme";
 import Input from "./Input"
 import { findByTestAttr } from "../../../test/testUtils"
@@ -15,5 +16,20 @@ describe("Input component", () => {
   test("render without error", () => {
     const component = findByTestAttr(wrapper, "component-input");
     expect(component.length).toBe(1);
+  })
+})
+
+describe("state controlled input field", () => {
+  test("state update with value of input box upon change", () => {
+    const mockSetCurrnetGuess = jest.fn()
+    React.useState = jest.fn(() => ["", mockSetCurrnetGuess]);
+
+    const wrapper = setup();
+    const inputBox = findByTestAttr(wrapper, "input-box");
+
+    const mockEvent = { target: { value: "train"} };
+    inputBox.simulate("change", mockEvent)
+
+    expect(mockSetCurrnetGuess).toHaveBeenCalledWith("train");
   })
 })
